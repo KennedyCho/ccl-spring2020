1fdaabf02a7c6b493c1258e47bc9e675b40db887
Kennedy Cambra-Cho <kennedy.cambracho@gmail.com>
2020-03-02 15:46:52 -1000

    edit js with animation

--- WEEK02/ANIMATION_HW/sketch.js
+++ WEEK02/ANIMATION_HW/sketch.js
@@ -1,14 +1,29 @@
 function setup(){
    createCanvas(windowWidth, windowHeight);
-   background(200);
+   background(255);
 
-   fill(0); 
-   rect(0, 0, windowWidth/2, windowHeight/2); 
-   rect(windowWidth, windowHeight, -windowWidth/2, -windowHeight/2);
-
-   fill(255);
-   ellipse(windowWidth/4, windowHeight/4, windowWidth/4, windowHeight/4);
-   ellipse(3*windowWidth/4, 3*windowHeight/4, windowWidth/4, windowHeight/4);
-
+   noStroke();
+   colorMode(RGB);
 }
 
+function draw(){  
+   
+   let colorX = map(mouseX, 0, windowWidth, 0, 255); 
+   let colorY = map(mouseY, 0, windowHeight, 0, 255); 
+     
+   // top left corner 
+   fill(colorX, mouseX/2+ (mouseX/2)* Math.sin(frameCount/50), colorY); 
+   rect(0, 0, mouseX+10, mouseY+10); 
+   // bottom right corner
+   fill(colorY, windowWidth/2+ (windowWidth/2)* Math.sin(frameCount/50), colorX); 
+   rect(windowWidth, windowHeight, -mouseX-10, -mouseY-10);
+
+   // circle moves left to right
+   fill(colorY, 0, colorX);
+   ellipse(windowWidth/2+ (windowWidth/2)* Math.sin(frameCount/50), (mouseY+10)/2, (mouseX)/2, (mouseY)/2);
+
+   // circle grows with bottom right corner box
+   fill(colorX, colorY, 0);
+   ellipse(windowWidth-mouseX/2, windowHeight-mouseY/2, mouseX/2+ (mouseX/2)* Math.sin(frameCount/50), mouseY/2+ (mouseY/2)* Math.sin(frameCount/50))
+  
+}
